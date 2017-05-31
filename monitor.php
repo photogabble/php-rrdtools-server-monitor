58dd1f71ea70a955d431c3308725ad1f7ec22814
Simon Dann <carbontwelve@users.noreply.github.com>
2017-05-31 12:01:15 +0100

    :sparkles: added configuration loading

--- monitor.php
+++ monitor.php
@@ -2,33 +2,11 @@
 date_default_timezone_set('GMT');
 require_once __DIR__ . "/vendor/autoload.php";
 
-$monitor = new \Carbontwelve\Monitor\Monitor([
-    'RRDCpu' => [
-        'enabled' => true,
-        'interval' => 1
-    ],
-    'RRDDiskIO' => [
-        'enabled' => true,
-        'interval' => 10,
-        'devices' => [
-            'nb0',
-            'nb1'
-        ]
-    ],
-    'RRDDiskUsage' => [
-        'enabled' => true,
-        'devices' => [
-            'nbd0' => '/dev/nbd0'
-        ]
-    ],
-    'RRDMemory' => [
-        'enabled' => true,
-    ],
-    'RRDNetwork' => [
-        'enabled' => true,
-    ],
-    'RRDNginx' => [
-        'enabled' => false,
-        'statsUrl' => 'http://127.0.0.1/nginx_status'
-    ]
-]);
+if (! file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'config.php')){
+    echo 'Cant find config.php in path ['. __DIR__ .']' . PHP_EOL;
+    exit(1);
+}
+
+$configuration = include __DIR__ . DIRECTORY_SEPARATOR . 'config.php';
+$monitor = new \Carbontwelve\Monitor\Monitor($configuration);
+$monitor->run();
--- src/monitor.php
+++ src/monitor.php
@@ -2,6 +2,7 @@
 
 namespace Carbontwelve\Monitor;
 
+use Carbontwelve\Monitor\Libs\View;
 use Carbontwelve\Monitor\Monitors\RRDBase;
 use Carbontwelve\Monitor\Monitors\RRDCpu;
 use Carbontwelve\Monitor\Monitors\RRDDiskIO;
@@ -28,10 +29,20 @@ Class Monitor {
     public function __construct(array $config = [])
     {
         $defaultConfig = [
-            'basePath' => __DIR__ . DIRECTORY_SEPARATOR . 'RRDStore',
+            'storagePath' => __DIR__ . DIRECTORY_SEPARATOR . 'RRDStore',
             'outputPath' => __DIR__ . DIRECTORY_SEPARATOR . 'public',
             'debug' => true
         ];
+
+        if (! file_exists($defaultConfig['storagePath'])) {
+            echo "Storage Path does not exist or is not writable." . PHP_EOL;
+            exit(1);
+        }
+
+        if (! file_exists($defaultConfig['outputPath'])) {
+            echo "Output Path does not exist or is not writable." . PHP_EOL;
+            exit(1);
+        }
 
         $config = array_merge($defaultConfig, $config);
 
@@ -41,7 +52,7 @@ Class Monitor {
 
         foreach ($this->monitors as $monitorName) {
             /** @var RRDBase $monitor */
-            $monitor = new $monitorName($defaultConfig['basePath'], $defaultConfig['debug']);
+            $monitor = new $monitorName($defaultConfig['storagePath'], $defaultConfig['debug']);
             $className = explode('\\', get_class($monitor));
             $className = end($className);
             $defaultMonitorConfig = [
@@ -86,7 +97,7 @@ Class Monitor {
         }
     }
 
-    private function generateGraphs($periods = ['hour', 'day', 'week', 'month', 'year']) {
+    private function generateGraphs($periods = []) {
         /** @var RRDBase $monitor */
         foreach ($this->monitors as $monitor) {
             foreach ($periods as $period) {
@@ -95,14 +106,27 @@ Class Monitor {
         }
     }
 
-    private function writeHTML() {
+    private function writeHTML($periods = []) {
+        $graphs = [];
 
+        /** @var RRDBase $monitor */
+        foreach ($this->monitors as $name => $monitor) {
+            $graphs[$name] = $monitor->getGraphName();
+        }
+
+        $view = new View(__DIR__ . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'dashboard.php');
+        file_put_contents($this->config['outputPath'] . DIRECTORY_SEPARATOR . 'index.html', $view->render([
+            'periods' => $periods,
+            'graphs' => $graphs,
+            'timestamp' => $this->timestamp
+        ]));
     }
 
     public function run() {
+        $periods = ['hour', 'day', 'week', 'month', 'year'];
         $this->touchGraphs();
         $this->collect();
-        $this->generateGraphs();
-        $this->writeHTML();
+        $this->generateGraphs($periods);
+        $this->writeHTML($periods);
     }
 }
